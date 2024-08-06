37eb0a33eac2645f7fb1346826a42de0628f0519
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-08-05 21:11:05 -0300

    Mejorando detalles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = ({ toggleLanguage, toggleDarkMode, language, darkMode, themeButtonText }) => {
   return (
-    <header className={`flex justify-between items-center p-4 ${darkMode ? 'bg-gray-800 text-white' : 'bg-gray-300 text-black'}`}>
+    <header className={`flex justify-between items-center p-4 ${darkMode ? 'bg-gray-800 text-white bg-opacity-80' : 'bg-gray-300 bg-opacity-80 text-black'}`}>
       <div>
         <button onClick={toggleLanguage} className="bg-blue-500 text-white px-4 py-2 rounded mr-4">
           {language === "es" ? "Switch to English" : "Cambiar a Español"}
