4902f7983dab6c9ea986a048d4e7ddddd992f4c1
Pablo Macieyko <macieyko.pablo@gmail.com>
2024-08-21 11:29:39 -0300

    ñ

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -23,7 +23,7 @@ const Body = ({ t, language, darkMode }) => {
             <div className="lg:w-1/2 flex justify-center items-center mt-6 lg:mt-0">
               <picture>
                 <img
-                  src="/portfolio.jpg"
+                  src="/Portfolio.jpg"
                   alt="Pablo Ivan Macieyko"
                   className="rounded-full max-w-full h-auto lg:w-60 lg:h-60 border-4 border-black object-cover"
                 />
